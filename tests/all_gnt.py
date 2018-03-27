57ad3249f968d36aeb1132fdd0b9b19db522e06e
Arg0s1080 <ivan.rincon76@gmail.com>
2018-03-27 20:41:45 +0200

    put tests in method

--- tests/all_gnt.py
+++ tests/all_gnt.py
@@ -8,11 +8,12 @@ from tests.td3_gnt_02 import TestCase05
 
 
 class TestCaseGntAll(unittest.TestCase):
-    TestCase01().test_td1_generator()
-    TestCase02().test_td1_generator()
-    TestCase03().test_td2_generator()
-    TestCase04().test_td3_generator()
-    TestCase05().test_td3_generator()
+    def test_generator(self):
+        TestCase01().test_td1_generator()
+        TestCase02().test_td1_generator()
+        TestCase03().test_td2_generator()
+        TestCase04().test_td3_generator()
+        TestCase05().test_td3_generator()
 
 
 if __name__ == '__main__':
