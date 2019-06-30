5a7e4f2992035859f521a843690de46f8eb771db
Arg0s1080 <ivan.rincon76@gmail.com>
2019-06-30 19:57:33 +0200

    add python >= 3.4 in classifiers

--- setup.py
+++ setup.py
@@ -29,6 +29,11 @@ setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Security"
     ]
 )
