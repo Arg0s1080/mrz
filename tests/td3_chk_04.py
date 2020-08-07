7af68be2c217f4c86df75d972f7d86a427f28e89
ivan <ivan.rincon76@gmail.com>
2020-08-07 05:32:01 +0200

    Update optional_data_hash TD3 unittest. See #21

--- tests/td3_chk_04.py
+++ tests/td3_chk_04.py
@@ -7,15 +7,18 @@ class TestCase20(unittest.TestCase):
     # if optional data is empty "0" and "<" will return True
     # See #21
 
-    def test_a_td3_checker(self):
-        mrz_td3 = ("P<CANMARTIN<<SARAH<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
-                   "ZE000509<9CAN8501019F2301147<<<<<<<<<<<<<<08")
-        self.assertTrue(bool(TD3CodeChecker(mrz_td3)))
+    def test_td3_checker(self):
+        # optional data hash == '0'
+        mrz1 = ("P<CANMARTIN<<SARAH<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
+                "ZE000509<9CAN8501019F2301147<<<<<<<<<<<<<<08")
+        test1 = bool(TD3CodeChecker(mrz1))
 
-    def test_b_td3_checker(self):
-        mrz_td3 = ("P<CANMARTIN<<SARAH<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
-                   "ZE000509<9CAN8501019F2301147<<<<<<<<<<<<<<<8")
-        self.assertTrue(bool(TD3CodeChecker(mrz_td3)))
+        # optional data hash == '<'
+        mrz2 = ("P<CANMARTIN<<SARAH<<<<<<<<<<<<<<<<<<<<<<<<<<\n"
+                "ZE000509<9CAN8501019F2301147<<<<<<<<<<<<<<08")
+        test2 = bool(TD3CodeChecker(mrz2))
+
+        self.assertEqual(test1, test2)
 
 
 if __name__ == '__main__':
