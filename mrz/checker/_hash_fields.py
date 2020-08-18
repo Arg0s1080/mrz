2c537fed00750bb09bd49d86505eac80e60e5879
ivan <ivan.rincon76@gmail.com>
2020-08-18 03:53:23 +0200

    Report change for _HashFieldsChecker:   - Remove _Report (parent inheritance)   - add self.report = _Report()   - Refactor self._report() by self.report.add()

--- mrz/checker/_hash_fields.py
+++ mrz/checker/_hash_fields.py
@@ -10,7 +10,7 @@ from ..base.functions import hash_is_ok
 from ._report import _Report
 
 
-class _HashChecker(_Report):
+class _HashChecker:
     def __init__(self, document_number: str, document_number_hash: str, birth_date: str, birth_date_hash: str,
                  expiry_date: str, expiry_date_hash: str):
         self._document_number = document_number
@@ -19,25 +19,26 @@ class _HashChecker(_Report):
         self._birth_date_hash = birth_date_hash
         self._expiry_date = expiry_date
         self._expiry_date_hash = expiry_date_hash
+        self.report = _Report()
 
     @property
     def document_number_hash(self) -> bool:
         """Return True if the hash of the document number is validated, False otherwise."""
 
-        return self._report("document number hash",
-                            hash_is_ok(self._document_number, self._document_number_hash))
+        return self.report.add("document number hash",
+                               hash_is_ok(self._document_number, self._document_number_hash))
 
     @property
     def birth_date_hash(self) -> bool:
         """Return True if the hash of the birth date is validated, False otherwise."""
 
-        return self._report("birth date hash", hash_is_ok(self._birth_date, self._birth_date_hash))
+        return self.report.add("birth date hash", hash_is_ok(self._birth_date, self._birth_date_hash))
 
     @property
     def expiry_date_hash(self) -> bool:
         """Return True if the hash of the expiry date is validated, False otherwise."""
 
-        return self._report("expiry date hash", hash_is_ok(self._expiry_date, self._expiry_date_hash))
+        return self.report.add("expiry date hash", hash_is_ok(self._expiry_date, self._expiry_date_hash))
 
     def _all_hashes(self):
         return (self.document_number_hash &
